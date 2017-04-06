eed990daee9a8050496a3356ccd27d014b2521cd
andy <andy.zhangtao@hotmail.com>
2017-04-06 10:21:05 +0800

    调整启动脚本
    Commit by Andy.Zhang
    Email: ztao8607@gmail.com

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-
-aria2c --enable-rpc --rpc-listen-all --conf-path=/root/.aria2/aria2.conf -D
-
 nginx
-
-tail -f /var/log/nginx/*
-
+touch aria2.session
+aria2c --enable-rpc --rpc-listen-all --conf-path=/root/.aria2/aria2.conf
